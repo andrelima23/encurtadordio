543f7bfe95198cb9cb028f986b2e8ebaa608ba2b
André Lima <dj_andre_lima@hotmail.com>
2021-11-11 04:14:52 -0300

    primeiro e ultimo commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION: 'mongodb+srv://andrelima22:iN9KG5nvMHX6RDw@encurtadordio.iharg.mongodb.net/encurtadordio?retryWrites=true&w=majority'
+};
+// iN9KG5nvMHX6RDw 
+ 
